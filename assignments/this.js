d87f5fabab0501d213d2dd32020bf247e6c73bae
Kira <kawoods826@gmail.com>
2019-08-13 19:42:32 -0700

    completed challenges for prototypes and this

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,53 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding
+* 2. Implicit Binding
+* 3. New Binding
+* 4. Explicit Binding
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function greet(name) {
+    console.log('Hello ' + name);
+    console.log(this);
+}
+
+greet('Kira');
 
 // Principle 2
 
-// code example for Implicit Binding
+var greet = {
+    greeting: 'Hello ',
+    speak: function(name) {
+        console.log(this.greeting + name);
+        console.log(this);
+    }
+}
+
+greet.speak('Kira');
 
 // Principle 3
 
-// code example for New Binding
+function bigHello(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting.toUpperCase() + this.greeter.toUpperCase());
+      console.log(this);
+    };
+  }
+  
+  const starsky = new bigHello('Starsky');
+  const hutch = new bigHello('Hutch');
+  
+  starsky.speak();
+  hutch.speak();
 
 // Principle 4
 
-// code example for Explicit Binding
+starsky.speak.call(hutch);
+hutch.speak.apply(starsky);
